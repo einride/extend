08d30d2d42b7b8e7e04e13eed1395d5c8ca1d1b2
Oscar Söderlund <odsod@users.noreply.github.com>
2022-09-28 21:20:56 +0200

    feat: upgrade to latest CLI generator

--- cmd/saga/tools.go
+++ cmd/saga/tools.go
@@ -5,6 +5,6 @@ package tools
 
 import (
 	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
-	_ "go.einride.tech/protoc-gen-go-cli"
+	_ "go.einride.tech/aip-cli/cmd/protoc-gen-go-aip-cli"
 	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 )
