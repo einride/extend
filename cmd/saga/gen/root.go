3c82df917af2f907277b292388edf8248cdf3b20
Oscar Söderlund <odsod@users.noreply.github.com>
2022-10-11 15:38:32 +0200

    feat: add saga parent package
    For consistency with other APIs in the Saga ecosystem.

--- cmd/saga/gen/root.go
+++ cmd/saga/gen/root.go
@@ -2,7 +2,7 @@
 package saga
 
 import (
-	v1beta1 "github.com/einride/saga/cmd/saga/gen/einride/extend/book/v1beta1"
+	v1beta1 "github.com/einride/saga/cmd/saga/gen/einride/saga/extend/book/v1beta1"
 	cobra "github.com/spf13/cobra"
 	aipcli "go.einride.tech/aip-cli/aipcli"
 )
