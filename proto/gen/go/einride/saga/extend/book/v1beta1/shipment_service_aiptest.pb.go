c65d80e22046fd64698361ed82c6041c7d3e2416
Hamsajj <hamid.sajjadi@einride.tech>
2023-03-20 16:37:14 +0100

    feat: update shipment api

--- proto/gen/go/einride/saga/extend/book/v1beta1/shipment_service_aiptest.pb.go
+++ proto/gen/go/einride/saga/extend/book/v1beta1/shipment_service_aiptest.pb.go
@@ -6,7 +6,9 @@ import (
 	context "context"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
+	proto "google.golang.org/protobuf/proto"
 	protocmp "google.golang.org/protobuf/testing/protocmp"
+	fieldmaskpb "google.golang.org/protobuf/types/known/fieldmaskpb"
 	assert "gotest.tools/v3/assert"
 	strings "strings"
 	testing "testing"
@@ -40,6 +42,9 @@ type ShipmentTestSuiteConfig struct {
 	// Create should return a resource which is valid to create, i.e.
 	// all required fields set.
 	Create func(parent string) *Shipment
+	// Update should return a resource which is valid to update, i.e.
+	// all required fields set.
+	Update func(parent string) *Shipment
 	// Patterns of tests to skip.
 	// For example if a service has a Get method:
 	// Skip: ["Get"] will skip all tests for Get.
@@ -50,6 +55,7 @@ type ShipmentTestSuiteConfig struct {
 func (fx *ShipmentTestSuiteConfig) test(t *testing.T) {
 	t.Run("Create", fx.testCreate)
 	t.Run("Get", fx.testGet)
+	t.Run("Update", fx.testUpdate)
 }
 
 func (fx *ShipmentTestSuiteConfig) testCreate(t *testing.T) {
@@ -340,6 +346,284 @@ func (fx *ShipmentTestSuiteConfig) testGet(t *testing.T) {
 			Name: "spaces/-/shipments/-",
 		})
 		assert.Equal(t, codes.InvalidArgument, status.Code(err), err)
+	})
+
+}
+
+func (fx *ShipmentTestSuiteConfig) testUpdate(t *testing.T) {
+	fx.maybeSkip(t)
+	// Method should fail with InvalidArgument if no name is provided.
+	t.Run("missing name", func(t *testing.T) {
+		fx.maybeSkip(t)
+		parent := fx.nextParent(t, false)
+		msg := fx.Update(parent)
+		msg.Name = ""
+		_, err := fx.service.UpdateShipment(fx.ctx, &UpdateShipmentRequest{
+			Shipment: msg,
+		})
+		assert.Equal(t, codes.InvalidArgument, status.Code(err), err)
+	})
+
+	// Method should fail with InvalidArgument if provided name is not valid.
+	t.Run("invalid name", func(t *testing.T) {
+		fx.maybeSkip(t)
+		parent := fx.nextParent(t, false)
+		msg := fx.Update(parent)
+		msg.Name = "invalid resource name"
+		_, err := fx.service.UpdateShipment(fx.ctx, &UpdateShipmentRequest{
+			Shipment: msg,
+		})
+		assert.Equal(t, codes.InvalidArgument, status.Code(err), err)
+	})
+
+	// Field update_time should be updated when the resource is updated.
+	t.Run("update time", func(t *testing.T) {
+		fx.maybeSkip(t)
+		parent := fx.nextParent(t, false)
+		created := fx.create(t, parent)
+		updated, err := fx.service.UpdateShipment(fx.ctx, &UpdateShipmentRequest{
+			Shipment: created,
+		})
+		assert.NilError(t, err)
+		assert.Check(t, updated.UpdateTime.AsTime().After(created.UpdateTime.AsTime()))
+	})
+
+	// The updated resource should be persisted and reachable with Get.
+	t.Run("persisted", func(t *testing.T) {
+		fx.maybeSkip(t)
+		parent := fx.nextParent(t, false)
+		created := fx.create(t, parent)
+		updated, err := fx.service.UpdateShipment(fx.ctx, &UpdateShipmentRequest{
+			Shipment: created,
+		})
+		assert.NilError(t, err)
+		persisted, err := fx.service.GetShipment(fx.ctx, &GetShipmentRequest{
+			Name: updated.Name,
+		})
+		assert.NilError(t, err)
+		assert.DeepEqual(t, updated, persisted, protocmp.Transform())
+	})
+
+	// The field create_time should be preserved when a '*'-update mask is used.
+	t.Run("preserve create_time", func(t *testing.T) {
+		fx.maybeSkip(t)
+		parent := fx.nextParent(t, false)
+		created := fx.create(t, parent)
+		originalCreateTime := created.CreateTime
+		updated, err := fx.service.UpdateShipment(fx.ctx, &UpdateShipmentRequest{
+			Shipment: created,
+			UpdateMask: &fieldmaskpb.FieldMask{
+				Paths: []string{
+					"*",
+				},
+			},
+		})
+		assert.NilError(t, err)
+		assert.DeepEqual(t, originalCreateTime, updated.CreateTime, protocmp.Transform())
+	})
+
+	parent := fx.nextParent(t, false)
+	created := fx.create(t, parent)
+	// Method should fail with NotFound if the resource does not exist.
+	t.Run("not found", func(t *testing.T) {
+		fx.maybeSkip(t)
+		msg := fx.Update(parent)
+		msg.Name = created.Name + "notfound"
+		_, err := fx.service.UpdateShipment(fx.ctx, &UpdateShipmentRequest{
+			Shipment: msg,
+		})
+		assert.Equal(t, codes.NotFound, status.Code(err), err)
+	})
+
+	// The method should fail with InvalidArgument if the update_mask is invalid.
+	t.Run("invalid update mask", func(t *testing.T) {
+		fx.maybeSkip(t)
+		_, err := fx.service.UpdateShipment(fx.ctx, &UpdateShipmentRequest{
+			Shipment: created,
+			UpdateMask: &fieldmaskpb.FieldMask{
+				Paths: []string{
+					"invalid_field_xyz",
+				},
+			},
+		})
+		assert.Equal(t, codes.InvalidArgument, status.Code(err), err)
+	})
+
+	// Method should fail with InvalidArgument if any required field is missing
+	// when called with '*' update_mask.
+	t.Run("required fields", func(t *testing.T) {
+		fx.maybeSkip(t)
+		t.Run(".sender", func(t *testing.T) {
+			fx.maybeSkip(t)
+			msg := proto.Clone(created).(*Shipment)
+			container := msg
+			if container == nil {
+				t.Skip("not reachable")
+			}
+			fd := container.ProtoReflect().Descriptor().Fields().ByName("sender")
+			container.ProtoReflect().Clear(fd)
+			_, err := fx.service.UpdateShipment(fx.ctx, &UpdateShipmentRequest{
+				Shipment: msg,
+				UpdateMask: &fieldmaskpb.FieldMask{
+					Paths: []string{
+						"*",
+					},
+				},
+			})
+			assert.Equal(t, codes.InvalidArgument, status.Code(err), err)
+		})
+		t.Run(".pickup_address.recipient", func(t *testing.T) {
+			fx.maybeSkip(t)
+			msg := proto.Clone(created).(*Shipment)
+			container := msg.GetPickupAddress()
+			if container == nil {
+				t.Skip("not reachable")
+			}
+			fd := container.ProtoReflect().Descriptor().Fields().ByName("recipient")
+			container.ProtoReflect().Clear(fd)
+			_, err := fx.service.UpdateShipment(fx.ctx, &UpdateShipmentRequest{
+				Shipment: msg,
+				UpdateMask: &fieldmaskpb.FieldMask{
+					Paths: []string{
+						"*",
+					},
+				},
+			})
+			assert.Equal(t, codes.InvalidArgument, status.Code(err), err)
+		})
+		t.Run(".pickup_address.line1", func(t *testing.T) {
+			fx.maybeSkip(t)
+			msg := proto.Clone(created).(*Shipment)
+			container := msg.GetPickupAddress()
+			if container == nil {
+				t.Skip("not reachable")
+			}
+			fd := container.ProtoReflect().Descriptor().Fields().ByName("line1")
+			container.ProtoReflect().Clear(fd)
+			_, err := fx.service.UpdateShipment(fx.ctx, &UpdateShipmentRequest{
+				Shipment: msg,
+				UpdateMask: &fieldmaskpb.FieldMask{
+					Paths: []string{
+						"*",
+					},
+				},
+			})
+			assert.Equal(t, codes.InvalidArgument, status.Code(err), err)
+		})
+		t.Run(".pickup_address.postal_code", func(t *testing.T) {
+			fx.maybeSkip(t)
+			msg := proto.Clone(created).(*Shipment)
+			container := msg.GetPickupAddress()
+			if container == nil {
+				t.Skip("not reachable")
+			}
+			fd := container.ProtoReflect().Descriptor().Fields().ByName("postal_code")
+			container.ProtoReflect().Clear(fd)
+			_, err := fx.service.UpdateShipment(fx.ctx, &UpdateShipmentRequest{
+				Shipment: msg,
+				UpdateMask: &fieldmaskpb.FieldMask{
+					Paths: []string{
+						"*",
+					},
+				},
+			})
+			assert.Equal(t, codes.InvalidArgument, status.Code(err), err)
+		})
+		t.Run(".pickup_address.city", func(t *testing.T) {
+			fx.maybeSkip(t)
+			msg := proto.Clone(created).(*Shipment)
+			container := msg.GetPickupAddress()
+			if container == nil {
+				t.Skip("not reachable")
+			}
+			fd := container.ProtoReflect().Descriptor().Fields().ByName("city")
+			container.ProtoReflect().Clear(fd)
+			_, err := fx.service.UpdateShipment(fx.ctx, &UpdateShipmentRequest{
+				Shipment: msg,
+				UpdateMask: &fieldmaskpb.FieldMask{
+					Paths: []string{
+						"*",
+					},
+				},
+			})
+			assert.Equal(t, codes.InvalidArgument, status.Code(err), err)
+		})
+		t.Run(".pickup_address.region_code", func(t *testing.T) {
+			fx.maybeSkip(t)
+			msg := proto.Clone(created).(*Shipment)
+			container := msg.GetPickupAddress()
+			if container == nil {
+				t.Skip("not reachable")
+			}
+			fd := container.ProtoReflect().Descriptor().Fields().ByName("region_code")
+			container.ProtoReflect().Clear(fd)
+			_, err := fx.service.UpdateShipment(fx.ctx, &UpdateShipmentRequest{
+				Shipment: msg,
+				UpdateMask: &fieldmaskpb.FieldMask{
+					Paths: []string{
+						"*",
+					},
+				},
+			})
+			assert.Equal(t, codes.InvalidArgument, status.Code(err), err)
+		})
+		t.Run(".delivery_address", func(t *testing.T) {
+			fx.maybeSkip(t)
+			msg := proto.Clone(created).(*Shipment)
+			container := msg
+			if container == nil {
+				t.Skip("not reachable")
+			}
+			fd := container.ProtoReflect().Descriptor().Fields().ByName("delivery_address")
+			container.ProtoReflect().Clear(fd)
+			_, err := fx.service.UpdateShipment(fx.ctx, &UpdateShipmentRequest{
+				Shipment: msg,
+				UpdateMask: &fieldmaskpb.FieldMask{
+					Paths: []string{
+						"*",
+					},
+				},
+			})
+			assert.Equal(t, codes.InvalidArgument, status.Code(err), err)
+		})
+		t.Run(".units", func(t *testing.T) {
+			fx.maybeSkip(t)
+			msg := proto.Clone(created).(*Shipment)
+			container := msg
+			if container == nil {
+				t.Skip("not reachable")
+			}
+			fd := container.ProtoReflect().Descriptor().Fields().ByName("units")
+			container.ProtoReflect().Clear(fd)
+			_, err := fx.service.UpdateShipment(fx.ctx, &UpdateShipmentRequest{
+				Shipment: msg,
+				UpdateMask: &fieldmaskpb.FieldMask{
+					Paths: []string{
+						"*",
+					},
+				},
+			})
+			assert.Equal(t, codes.InvalidArgument, status.Code(err), err)
+		})
+		t.Run(".vehicle.reference_id", func(t *testing.T) {
+			fx.maybeSkip(t)
+			msg := proto.Clone(created).(*Shipment)
+			container := msg.GetVehicle()
+			if container == nil {
+				t.Skip("not reachable")
+			}
+			fd := container.ProtoReflect().Descriptor().Fields().ByName("reference_id")
+			container.ProtoReflect().Clear(fd)
+			_, err := fx.service.UpdateShipment(fx.ctx, &UpdateShipmentRequest{
+				Shipment: msg,
+				UpdateMask: &fieldmaskpb.FieldMask{
+					Paths: []string{
+						"*",
+					},
+				},
+			})
+			assert.Equal(t, codes.InvalidArgument, status.Code(err), err)
+		})
 	})
 
 }
